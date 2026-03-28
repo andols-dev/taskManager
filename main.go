ef0f5d0bf8744a07137a6e955c112bb8561a801c
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-28 13:29:58 +0100

    create menu

--- main.go
+++ main.go
@@ -3,5 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to the Task Manager!")
+	// select options from a menu
+	menu()
 }
+
+func menu() {
+	fmt.Println("Welcome to the Task Manager!")
+	fmt.Println("Please select an option:")
+	fmt.Println("1. Add Task")
+	fmt.Println("2. View Tasks")
+	fmt.Println("3. Complete Task")
+	fmt.Println("4. Exit")
+	var input string
+	fmt.Scanln(&input)
+	// while
+	for input != "4" {
+		switch input {
+		case "1":
+			fmt.Println("Adding a new task...")
+		case "2":
+			fmt.Println("Viewing tasks...")
+		case "3":
+			fmt.Println("Completing a task...")
+		default:
+			fmt.Println("Invalid option, please try again.")
+		}
+		fmt.Println("Please select an option:")
+		fmt.Scanln(&input)
+	}
+
+}
+
+/* func addTask() {
+	fmt.Println("Adding a new task...")
+} */
