168585f0d51c763c4606f014a75849b8de95cc3a
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-03-28 13:35:45 +0100

    create task.go

---
+++ task.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func menu() {
+	fmt.Println("Welcome to the Task Manager!")
+	fmt.Println("Please select an option:")
+	fmt.Println("1. Add Task")
+	fmt.Println("2. View Tasks")
+	fmt.Println("3. Complete Task")
+	fmt.Println("4. Exit")
+	var input string
+	fmt.Scanln(&input)
+	// while
+	for input != "4" {
+		switch input {
+		case "1":
+			fmt.Println("Adding a new task...")
+		case "2":
+			fmt.Println("Viewing tasks...")
+		case "3":
+			fmt.Println("Completing a task...")
+		default:
+			fmt.Println("Invalid option, please try again.")
+		}
+		fmt.Println("Please select an option:")
+		fmt.Scanln(&input)
+	}
+
+}
